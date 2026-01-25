a05e69ea4da3ed0d56df22b520188800d5239edb
DeathPlayer0456 <jsjzskksskjjsks@gmail.com>
2026-01-25 18:35:02 +0800

    Update presets.lua

--- src/presets.lua
+++ src/presets.lua
@@ -124,7 +124,7 @@ return {
         -- The default LuaVersion is Lua51
         LuaVersion = "Lua51";
         -- For minifying no VarNamePrefix is applied
-        VarNamePrefix = "IiiiiiiiIiieIiIiiIIiiiiIIeIIIIIIIiiIiiiiiiiiiieiiiiiiiiiii";
+        VarNamePrefix = "l";
         -- Name Generator for Variables that look like this: IlI1lI1l
         NameGenerator = "MangledShuffled";
         -- No pretty printing
